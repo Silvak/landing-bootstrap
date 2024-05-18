285c198a874c2f438cb08d16e3a78fce3a89d3b1
silvak <silvak.jeg@gmail.com>
2024-05-17 23:16:23 -0400

    add hero

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,35 @@
 import "../css/styles.scss";
+
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+    var recipient = "silvak.jeg@gmail.com";
+
+    var mailtoLink =
+      "mailto:" +
+      recipient +
+      "?subject=" +
+      encodeURIComponent(subject) +
+      "&body=" +
+      encodeURIComponent(
+        "Name: " +
+          name +
+          "\n" +
+          "Email: " +
+          email +
+          "\n" +
+          "Phone: " +
+          phone +
+          "\n" +
+          "Message: " +
+          message
+      );
+    window.location.href = mailtoLink;
+  });
